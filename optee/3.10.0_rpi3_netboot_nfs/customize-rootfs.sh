f116c82ad01df31540a3eb21a488994d233c4a63
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2022-09-19 21:44:22 +0900

    Enable network before apt update

--- optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
+++ optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
@@ -3,17 +3,18 @@
 USER_DIR=/home/user
 ROOTFS_EXT_DIR=${USER_DIR}/arm64-20.04-rootfs
 
+# Start the service to enable network
+# Must be before and network access
+chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'systemctl enbale systemd-networkd'
+# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'
+
 # Tamproto requires newer npm and nodejs
-chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'apt-get update'
+chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'systemctl start systemd-networkd && apt-get update'
 chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'apt-get install -y npm'
 chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash'
 chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'source $HOME/.nvm/nvm.sh && nvm install --lts && nvm use --lts'
 # chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'nvm install --lts'
 # chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'nvm use --lts'
-
-# Start the service to enable network 
-chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'systemctl enbale systemd-networkd'
-# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'
 
 # Update rootfs tar file
 cd ${USER_DIR}

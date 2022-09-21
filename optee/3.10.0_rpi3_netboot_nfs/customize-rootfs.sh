6e9eb4899567957266f247848e21495141e723d0
qsfix-nagaraja <nagarajat@cabcsgroup-india.com>
2022-09-21 17:43:57 +0900

    Formatted some steps to copy and cleaned up

--- optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
+++ optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
@@ -9,14 +9,7 @@ cp /mnt/20-wired.network ${ROOTFS_EXT_DIR}/etc/systemd/network/
 chmod 644 ${ROOTFS_EXT_DIR}/etc/systemd/network/20-wired.network
 
 chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'systemctl enable systemd-networkd'
-# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'
-
-# Tamproto requires newer npm and nodejs
-# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'apt-get update && apt-get install -y npm'
-chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'apt-get update && curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash'
-chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'source $HOME/.nvm/nvm.sh && nvm install --lts && nvm use --lts'
-# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'nvm install --lts'
-# chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'nvm use --lts'
+chroot ${ROOTFS_EXT_DIR} /bin/bash -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf' && 'chattr +i /etc/resolv.conf'
 
 # Update rootfs tar file
 cd ${USER_DIR}
--- optee/3.10.0_rpi3_netboot_nfs/install_node.sh
+++ optee/3.10.0_rpi3_netboot_nfs/install_node.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -xue
 
-# Update repository and install npm
-sudo apt-get update && sudo apt-get install -y npm
-
 # Install nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Source the nvm scripts
+source $HOME/.nvm/nvm.sh
+
+#install NVM and set the latest version
+nvm install --lts && nvm use --lts

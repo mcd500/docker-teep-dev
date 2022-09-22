761bde2c4404e3a53f80df77be82d84708558cfc
vignesh-cabcsgroup <vigneshra@cabcsgroup-india.com>
2022-09-22 11:13:11 +0530

    install_node.sh: make single command. Dockerfile: add x permission to install_node.sh

--- optee/3.10.0_rpi3_netboot_nfs/install_node.sh
+++ optee/3.10.0_rpi3_netboot_nfs/install_node.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -xue
 
-# Install nvm
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-# Source the nvm scripts
-source $HOME/.nvm/nvm.sh
-
-#install NVM and set the latest version
-nvm install --lts && nvm use --lts
+# Install nvm, Source the nvm scripts and set nvm latest version
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source $HOME/.nvm/nvm.sh && nvm install --lts && nvm use --lts

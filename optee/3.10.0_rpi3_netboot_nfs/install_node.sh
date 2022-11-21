eea768e01ba9bbe119ac890257dd78f8d3af3e9d
qsfix-nagaraja <nagarajat@cabcsgroup-india.com>
2022-11-21 14:33:28 +0900

    Made the following changes 1. Changed the IP to local to test the teep-device in RPI3 2. Added the script to copy suit and TA into tamproto folder 3. Added the step to change the nameserver in install_node.sh 4. Copied 2 TA's for teep-agent and hello_tc into lib/optee_armtz

--- optee/3.10.0_rpi3_netboot_nfs/install_node.sh
+++ optee/3.10.0_rpi3_netboot_nfs/install_node.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Change the IP address in the resolv.conf
+# This step is not required if DNS server is working properly in RPI3
+sed -i -e 's/127.0.0.53/192.168.100.1/g' /etc/resolv.conf 
+
 # Install nvm, Source the nvm scripts and set nvm latest version
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
